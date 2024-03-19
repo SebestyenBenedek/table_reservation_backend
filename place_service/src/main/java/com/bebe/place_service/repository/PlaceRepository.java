88ac3f9014c9da51bc915bbe24e5925bdde9faa5
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:16:54 +0100

    add PlaceRepository interface

---
+++ place_service/src/main/java/com/bebe/place_service/repository/PlaceRepository.java
@@ -0,0 +1,10 @@
+package com.bebe.place_service.repository;
+
+import com.bebe.place_service.model.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Place findPlaceById(Long placeId);
+}
