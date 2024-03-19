9932dc5811d8d3dd7fbda23aef48280917567adc
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:11:29 +0100

    implement abstract User class

---
+++ place_service/src/main/java/com/bebe/place_service/model/user/User.java
@@ -0,0 +1,33 @@
+package com.bebe.place_service.model.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(unique = true)
+    private String userName;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private String profilePictureUrl;
+
+    private String language;
+}
