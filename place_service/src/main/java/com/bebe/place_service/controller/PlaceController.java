a7566b827a31b0b99fd26df8f4e42769646051b9
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:17:41 +0100

    implement PlaceController class

---
+++ place_service/src/main/java/com/bebe/place_service/controller/PlaceController.java
@@ -0,0 +1,51 @@
+package com.bebe.place_service.controller;
+
+import com.bebe.place_service.dto.NewPlaceDTO;
+import com.bebe.place_service.dto.PlaceDTO;
+import com.bebe.place_service.model.Place;
+import com.bebe.place_service.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+    private final PlaceService placeService;
+
+    @Autowired
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/")
+    public Set<Place> fetchPlaceList() {
+        return placeService.getAllPLaces();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Place> fetchPlaceById(@PathVariable("id") Long placeId) {
+        return ResponseEntity.ok(placeService.getPlaceById(placeId));
+    }
+
+    @PostMapping("/")
+    public Place savePlace(@Validated @RequestBody NewPlaceDTO place) {
+        return placeService.addPlace(place);
+    }
+
+    @PutMapping("/{id}")
+    public Place updatePlace(@RequestBody PlaceDTO placeDTO, @PathVariable("id") Long placeId) {
+        return placeService.updatePlace(placeDTO, placeId);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletePlaceById(@PathVariable("id") Long placeId) {
+        placeService.deletePlaceById(placeId);
+
+        return "Deleted successfully";
+    }
+}
