d5e0a8b064eebcab6284aca0d44dd9d15edfc601
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:23:25 +0100

    refactor eureka connection

---
+++ eureka_server/src/test/java/com/bebe/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebe.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
