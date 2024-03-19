9f6ef1cc7386d628293fa991164da460c939f3eb
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:12:29 +0100

    implement PlaceTable model class

---
+++ place_service/src/main/java/com/bebe/place_service/model/PlaceTable.java
@@ -0,0 +1,37 @@
+package com.bebe.place_service.model;
+
+import com.bebe.place_service.model.reservation.Reservation;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class PlaceTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Place place;
+
+    @OneToMany(mappedBy = "placeTable")
+    private Set<TimeInterval> timeIntervals;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlaceTable that = (PlaceTable) o;
+        return id == that.id && Objects.equals(place, that.place) && Objects.equals(timeIntervals, that.timeIntervals);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, place, timeIntervals);
+    }
+}
