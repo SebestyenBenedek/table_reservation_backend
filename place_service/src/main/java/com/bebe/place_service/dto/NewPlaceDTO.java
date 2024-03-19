702cc811efb1e3a5d6021bc09645998e3e77aca1
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-19 14:13:46 +0100

    add NewPlaceDTO record

---
+++ place_service/src/main/java/com/bebe/place_service/dto/NewPlaceDTO.java
@@ -0,0 +1,20 @@
+package com.bebe.place_service.dto;
+
+import com.bebe.place_service.model.Characteristic;
+import com.bebe.place_service.model.user.AdminUser;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Set;
+
+@Getter
+public record NewPlaceDTO(String name,
+                          String address,
+                          String description,
+                          String menu,
+                          List<Characteristic> characteristics,
+                          Set<String> images,
+                          int numberOfTables,
+                          AdminUser owner,
+                          TimeIntervalForWeekDTO timeIntervalForWeek) {
+}
