4124480d231826b58d5bbe1abc28e528c612eff8
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-20 14:55:26 +0100

    implement GuestUserController class

---
+++ guest_user_service/src/main/java/com/bebe/guest_user_service/controller/GuestUserController.java
@@ -0,0 +1,33 @@
+package com.bebe.guest_user_service.controller;
+
+import com.bebe.guest_user_service.model.GuestUser;
+import com.bebe.guest_user_service.service.GuestUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class GuestUserController {
+    private final GuestUserService guestUserService;
+
+    @Autowired
+    public GuestUserController(GuestUserService guestUserService) {
+        this.guestUserService = guestUserService;
+    }
+
+    @PostMapping("/")
+    public GuestUser saveUser(@Validated @RequestBody GuestUser user) {
+        return guestUserService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public GuestUser updateUser(@RequestBody GuestUser user, @PathVariable("id") Long userId) {
+        return guestUserService.updateUser(user, userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable("id") Long userId) {
+        guestUserService.deleteUserById(userId);
+    }
+}
