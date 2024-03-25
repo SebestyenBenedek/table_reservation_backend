2bc6fc05be0b84f4f8aceef9bb27b05e75932fcb
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-25 18:20:09 +0100

    refactor table generator files

--- place_service/src/main/java/com/bebe/place_service/service/factory/placeTable/PlaceTableGeneratorImpl.java
+++ place_service/src/main/java/com/bebe/place_service/service/factory/placeTable/PlaceTableGeneratorImpl.java
@@ -2,7 +2,6 @@ package com.bebe.place_service.service.factory.placeTable;
 
 import com.bebe.place_service.model.Place;
 import com.bebe.place_service.model.PlaceTable;
-import com.bebe.place_service.model.TimeInterval;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -11,13 +10,13 @@ import java.util.Set;
 @Component
 public class PlaceTableGeneratorImpl implements PlaceTableGenerator {
     @Override
-    public Set<PlaceTable> generatePlaceTable(int numberOfTables, Place place, Set<TimeInterval> timeIntervals) {
+    public Set<PlaceTable> generatePlaceTable(int numberOfTables, Place place) {
         Set<PlaceTable> newPlaceTables = new HashSet<>();
 
         for (int i = 0; i <= numberOfTables; i++) {
             PlaceTable placeTable = new PlaceTable();
             placeTable.setPlace(place);
-            placeTable.setTimeIntervals(timeIntervals);
+            placeTable.setTimeIntervals(new HashSet<>());
             newPlaceTables.add(placeTable);
         }
 
