a38204525fa378a9dbc75ccffe7c84c358c41935
SebestyenBenedek <sebi.bebe@gmail.com>
2024-03-26 22:31:32 +0100

    implement TimeInterval generator for generate entities for a month

--- place_service/src/main/java/com/bebe/place_service/service/factory/timeInterval/TimeIntervalGenerator.java
+++ place_service/src/main/java/com/bebe/place_service/service/factory/timeInterval/TimeIntervalGenerator.java
@@ -1,14 +1,16 @@
 package com.bebe.place_service.service.factory.timeInterval;
 
 
+import com.bebe.place_service.dto.TimeIntervalForDayDTO;
 import com.bebe.place_service.model.Place;
 import com.bebe.place_service.model.PlaceTable;
 import com.bebe.place_service.model.TimeInterval;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
 
 public interface TimeIntervalGenerator {
-    Set<TimeInterval> generateTimeInterval(DayOfWeek day, LocalTime openingHour, LocalTime closingHour, PlaceTable table);
+    Set<TimeInterval> generateTimeInterval(Set<TimeIntervalForDayDTO> timeIntervalForDayDTOSet, LocalDate currentDate, PlaceTable table);
 }
