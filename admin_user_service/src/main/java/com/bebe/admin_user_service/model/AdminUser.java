28c1fea2214fad094eddc4741ce34006df78dcd1
SebestyenBenedek <sebi.bebe@gmail.com>
2024-05-28 10:26:31 +0200

    refactor AdminUser class

--- admin_user_service/src/main/java/com/bebe/admin_user_service/model/AdminUser.java
+++ admin_user_service/src/main/java/com/bebe/admin_user_service/model/AdminUser.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Entity
 @Getter
 @Setter
+@Accessors(chain = true)
 public class AdminUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
